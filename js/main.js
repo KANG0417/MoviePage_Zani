c5948889b6f0dd2a2b6f3d6e23357a738382afd5
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2023-10-24 20:35:21 +0900

    유효성검사 기능 추가하기

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const homeBtn = document.querySelector("#header h1");
 // 이미지를 클릭하면 알림창에 아이디값 뿌려주기
 const imgClick = (title, id) => {
   alert(`(영화 id) ${id}, (영화 제목) ${title}`);
-}
+};
 
 // API 가져오기
 const API_KEY = "6058bbe97cc057987d5101e107d2daa2";
@@ -16,44 +16,42 @@ const topRateUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY
 // 영화 조회 함수 생성
 const getMovie = (url) => {
   fetch(url)
-  .then(response => {
-    return response.json();
-  })
-  .then(data =>  {
-    const movies = data.results;
-    movies.forEach(item => {
-      const {
-        title,
-        overview,
-        vote_average,
-          backdrop_path,
-          id,
-        } = item;
-        
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const movies = data.results;
+      movies.forEach((item) => {
+        const { title, overview, vote_average, backdrop_path, id } = item;
+
         let imgUrl = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
-        let noImg = "./img/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg";
+        let noImg =
+          "./img/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg";
         let cardHtml = `
         <div id="movie-card-wrap">
           <div class="img">
-            <img src="${backdrop_path !== null ? imgUrl : noImg}" alt="영화 이미지"
+            <img src="${
+              backdrop_path !== null ? imgUrl : noImg
+            }" alt="영화 이미지"
             onclick="imgClick('${title}', ${id})" class="movie-img"></img>
           </div>
           <p class="movie-title">${title}</p>
           <p class="movie-content">${overview}</p>
           <div class="rating">
-            <p class="rating-icon"><span class="movie-rating">${Math.ceil(vote_average * 10) / 10}</span></p>
+            <p class="rating-icon"><span class="movie-rating">${
+              Math.ceil(vote_average * 10) / 10
+            }</span></p>
             </div>
         </div>
         `;
-        
+
         if (overview !== "") {
           card.insertAdjacentHTML("beforeend", cardHtml);
         }
       });
-
     })
-    .catch(err => console.error(err));
-}
+    .catch((err) => console.error(err));
+};
 
 // 전체 목록 조회
 getMovie(topRateUrl);
@@ -64,20 +62,21 @@ homeBtn.addEventListener("click", (e) => {
   card.innerHTML = "";
   getMovie(topRateUrl);
 });
-  
+
 // 버튼을 클릭하거나 엔터를 치면 검색 기능
-searchForm.addEventListener("submit", async(e) => {
+searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const inputVal = document.querySelector("#search-box .search-txt").value.toLowerCase();
+  const inputVal = document
+    .querySelector("#search-box .search-txt")
+    .value.toLowerCase();
   const queryUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${inputVal}&include_adult=false&language=ko-KR&page=1`;
 
   if (inputVal.trim() === "") {
-    alert("검색어를 입력해주세요!")
+    alert("검색어를 입력해주세요!");
     return false;
   }
   if (queryUrl.includes(inputVal)) {
-    console.log("일치합니다!");
     // 영화 목록 초기화
     card.innerHTML = "";
     // 검색 목록 조회
