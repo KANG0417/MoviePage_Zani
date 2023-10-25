f412e9b9b3cbb0503262d8f4c8a4b1496b177c44
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2023-10-25 12:02:02 +0900

    유효성 검사 2차 수정

--- js/detail.js
+++ js/detail.js
@@ -37,7 +37,23 @@ nameInput.addEventListener("keyup", function () {
     nameResult.style.color = "lightcoral";
   }
 });
-// 2. 작성자,비밀번호 최종 유효성 검사
+
+// 2. 작성자(한글) 유효성 검사
+const passwordInput = document.getElementById("passwordInput");
+const passwordResult = document.getElementById("passwordResult");
+passwordInput.addEventListener("keyup", function () {
+  const regPassword = /^[0-9]{4}$/;
+
+  if (regPassword.test(passwordInput.value)) {
+    passwordResult.innerHTML = "✔";
+    passwordResult.style.color = "lightseagreen";
+  } else {
+    passwordResult.innerHTML = "4개의 숫자만 입력하세요.";
+    passwordResult.style.color = "lightcoral";
+  }
+});
+
+// 3. 비밀번호(숫자갯수) 유효성 검사
 function validate() {
   const nameInput = document.getElementById("nameInput");
   const regName = /^[가-힣]{2,5}$/;
